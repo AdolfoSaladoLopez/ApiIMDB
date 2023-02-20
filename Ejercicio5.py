e1d60aa5456fb70b6e2c5a99c5956f665f602752
AdolfoSaladoLopez <adolfo.s255446@cesurformacion.com>
2023-02-20 13:52:25 +0100

    Cambios.

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -7,7 +7,9 @@ r = requests.get('https://imdb-api.com/en/API/SearchTitle/k_n4fp5ybj/Akira%20198
 
 pelicula = r.json()["results"][0]
 imagen = pelicula["image"]
-imagen_resolucion = 'https://m.media-amazon.com/images/M/MV5BM2ZiZTk1ODgtMTZkNS00NTYxLWIxZTUtNWExZGYwZTRjODViXkEyXkFqcGdeQXVyMTE2MzA3MDM@._V1_UX2000_CR0,660,2000,3000_AL_.jpg'
+imagen_resolucion = 'https://m.media-amazon.com/images/M' \
+                    '/MV5BM2ZiZTk1ODgtMTZkNS00NTYxLWIxZTUtNWExZGYwZTRjODViXkEyXkFqcGdeQXVyMTE2MzA3MDM' \
+                    '@._V1_UX2000_CR0,660,2000,3000_AL_.jpg'
 reporte = 'https://imdb-api.com/en/API/Report/k_n4fp5ybj/tt0094625'
 
 print("La imagen de la película original es: " + str(imagen))
